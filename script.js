eb88c8967dc48bded83689a1e2e59fd896caf3aa
Sergey Boronin <bo-ronin@yandex.ru>
2022-04-28 13:58:23 +0300

    добавил верстку и css

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-'use strict';
+const scores = [0, 0]
+let roundScore = 0;
+let activePlayer = 0
+
+
+
+
+
+const btnRollDice = document.querySelector('.btn--roll')
+const diceElement = document.querySelector('.dice')
+diceElement.classList.add('dice--hidden')
+
+function showDice(dice) {
+  diceElement.classList.remove('dice--hidden')
+  diceElement.src = `dice-${dice}.png`
+}
+
+btnRollDice.addEventListener('click', function () {
+  let currentScore = document.querySelector(`#current--${activePlayer}`)
+  const dice = Math.ceil(Math.random() * 6)
+  showDice(dice)
+  
+  if(dice !== 1 ) {
+    roundScore += dice
+  } else {
+
+  }
+}) 
